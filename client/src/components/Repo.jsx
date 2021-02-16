a024b1e6005e85aaf3bdeeca2c6d3ff303cf4474
galexy n <alexneeeeewin@icloud.com>
2021-02-15 18:34:03 -0800

    [feat] added url link to username

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -4,10 +4,10 @@ const Repo = (props) =>
 (
   <div className="repo-item--container">
     <div className="repo-item-div-container">
-      <h5 className="repo-item--items">{props.repo.owner}:{props.repo.owner_id}</h5>
+      <h5 className="repo-item--items"><a href={`https://www.github.com/${props.repo.owner}`} target="_blank">{props.repo.owner}:{props.repo.owner_id}</a></h5>
     </div>
     <div className="repo-item-div-container">
-      <h5 className="repo-item--items"><a href={props.repo.html_url}>{props.repo.repo_name}:{props.repo.repo_id}</a></h5>
+      <h5 className="repo-item--items"><a href={props.repo.html_url} target="_blank">{props.repo.repo_name}:{props.repo.repo_id}</a></h5>
     </div>
     <div className="repo-item-div-container">
       <h5 className="repo-item--items">{props.repo.total_impression_count}</h5>
